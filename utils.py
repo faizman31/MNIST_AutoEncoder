132d6e0b86ba9f5d8ee50579ff42f32ac6ef5857
DJ Shin <faizman31@gmail.com>
2023-01-18 19:09:08 +0900

    added load_mnist

--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
 import torch
 import numpy as np
 
+def load_mnist(is_train,flatten=True):
+    from torchvision import datasets,transforms
+    dataset = datasets.mnist('../data',
+    train=is_train,
+    download=True,
+    transform=transforms.Compose([transforms.ToTensor()])
+    )
+
+    x = dataset.data.float() / 255. # min/max Scaling
+    y = dataset.target
+
+    if flatten:
+        x=x.reshape(x.shape[0],-1)
+
+    return x,y
+
+
 def get_encoder_hidden_size(n_layers):
     hidden_sizes=[]
     current_size=784 # MNIST 28*28 = 784

7ba4c11e15961a19e48adc54e1bbab1cad790f99
DJ Shin <faizman31@gmail.com>
2023-01-18 17:04:37 +0900

    added _train in Trainer.py

--- trainer.py
+++ trainer.py
@@ -21,11 +21,31 @@ class Trainer():
     def _train(self,x,y,config):
         self.model.train()
         # Shuffle before train
-        indices = torch.randperm(x.shape[0])
+        indices = torch.randperm(x.shape[0],device=x.device)
         x = torch.index_select(x,dim=0,index=indices).split(config.batch_size,dim=0)
         y = torch.index_select(y,dim=0,index=indices).split(config.batch_size,dim=0)
 
         total_loss = 0 
+
+        for i,(x_i,y_i) in enumerate(zip(x,y)):
+            y_hat_i = self.model(x_i)
+            loss_i= self.crit(y_hat_i,y_i.squeeze())
+
+            self.optimizer.zero_grad()
+            loss_i.backward()
+            
+            self.optimizer.step()
+
+            if config.verbose >= 2:
+                print('Train Iteration(%d/%d) : loss=%.4e' %(i+1,len(x),loss_i))
+
+            total_loss = float(loss_i)
+        
+        return total_loss / len(x)
+
+
+
+            
 
 
 
